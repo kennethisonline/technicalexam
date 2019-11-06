2b3634331dcbff1da5975f7fa80bf488c91ce38b
kenne <kennethisonline@yahoo.com>
2019-11-06 22:35:42 +0800

    Initial commit for technical exam

---
+++ src/main/java/com/kenneth/technicalexam/TechnicalexamApplication.java
@@ -0,0 +1,13 @@
+package com.kenneth.technicalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalexamApplication.class, args);
+	}
+
+}
